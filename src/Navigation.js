62712455fd198c78e7069678df748daae2ce8697
Stephanie Simms <stephanie.simms@gmail.com>
2019-08-06 20:05:45 -0700

    added navigation bar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import { NavLink } from 'react-router-dom';
 import { Navbar, Nav, NavItem } from 'reactstrap';
+import "./Navigation.css";
 
 class Navigation extends Component {
   render() {
     return (
       <div>
-        
+        <Navbar expand="md">
+          <NavLink exact to="/" className="navbar-brand">
+            Shoply
+          </NavLink>
+
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink to="/products/">Products</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/cart/">Cart</NavLink>
+            </NavItem>
+          </Nav>
+
+        </Navbar>
       </div>
     );
   }
@@ -14,18 +29,3 @@ class Navigation extends Component {
 
 export default Navigation;
 
-{/* <Navbar expand="md">
-  <NavLink exact to="/" className="navbar-brand">
-    Shoply
-          </NavLink>
-
-  <Nav className="ml-auto" navbar>
-    <NavItem>
-      <NavLink to="/products/">Products</NavLink>
-    </NavItem>
-    <NavItem>
-      <NavLink to="/cart/">Cart</NavLink>
-    </NavItem>
-  </Nav>
-
-</Navbar> */}
